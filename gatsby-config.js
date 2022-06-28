8839d7f8ed4ae823b6bd1b9597e21814c5a0c04d
lizzieshipwreck <lizzieshipwreck@gmail.com>
2022-06-27 19:16:34 -0700

    ftr: added images and plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     title: `Landing Page`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ],
 }
