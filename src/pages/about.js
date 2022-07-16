db9541c6ab2f8c014e2d33be37cd4d5717b421a3
lizzieshipwreck <lizzieshipwreck@gmail.com>
2022-07-15 21:32:10 -0700

    fix: bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,11 +12,12 @@ const AboutPage = () => {
           <div className={styles.textBlock}>
             <h2 className={styles.textHeader}>I'm a part-time author and a full-time pirate 🪶</h2>
             <p className={styles.text}>
-              Hi! I'm Liz. I write speculative fiction about badass young women. I like books where romance takes a back seat,
-              or just stays home. I enjoy creative world-building, humor, and characters who are nearly irredeemable. 
+              Hi! I'm Liz. I have a stupid haircut and I write speculative fiction about young women who make questionable decisions.
+              I like books where romance takes a back seat, or just stays home.
+              I enjoy creative world-building, humor, and characters who are nearly irredeemable. 
             </p>
             <p className={styles.text}>
-              I live on a 43ft sailboat with my partner and my dog. We're on a mission to sail around
+              I live on a sailboat with my partner and my dog. We're on a mission to sail around
               the world! I strive to incorporate my sailing experiences into my writing while
               being sensitive to the people who occupy the places we travel to.
             </p>
