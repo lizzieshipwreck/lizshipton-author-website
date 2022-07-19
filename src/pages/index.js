49e71b68d153bab04ceb633eb658978bfd761a91
lizzieshipwreck <lizzieshipwreck@gmail.com>
2022-07-19 09:46:26 -0700

    ftr: add new scourge mockup

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,16 @@ const IndexPage = () => {
   return (
     <Layout pageTitle={"Home"}>
       <div className={styles.content}>
-        <Link to="https://subscribepage.io/scourge-giveaway">
-          <h1 className={styles.text}><i>Scourge</i> novella available now</h1>
+        <a href="https://subscribepage.io/scourge-giveaway">
+          <h1 className={styles.titleText}><b>Scourge</b> novella available now</h1>
           <StaticImage
-            src="../images/salt_mockup.jpg"
+            src="../images/scourge_mockup.jpg"
             width={1000}
             placeholder="blurred"
             quality={100}
           />
           <h2 className={styles.text}>Click to download for FREE!</h2>
-        </Link>
+        </a>
       </div>
     </Layout>
   )
