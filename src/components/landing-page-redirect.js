a322778d96645288d20fb5ec601ed4b24094c07f
lizzieshipwreck <lizzieshipwreck@gmail.com>
2022-07-22 15:24:01 -0700

    fix: redirect render

--- src/components/landing-page-redirect.js
+++ src/components/landing-page-redirect.js
@@ -1,15 +1,19 @@
 import * as React from 'react'
+import { useState } from 'react'
 import { useEffect } from 'react'
 import QueryString from 'qs'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import * as styles from './landing-page-redirect.module.css'
 
-const LandingPageRedirect = ({pageTitle, redirectUrl}) => {
+const LandingPageRedirect = ({ pageTitle, redirectUrl }) => {
+    
+    const [params, setParams] = useState({});
 
     useEffect(() => {
         if (typeof window !== undefined) {
             const params = QueryString.parse(window.location.search, { ignoreQueryPrefix: true });
+            setParams(params)
             if (!params['thank_you']) {
                 redirectToLandingPage();
             }
@@ -24,14 +28,16 @@ const LandingPageRedirect = ({pageTitle, redirectUrl}) => {
     }
 
     return (
-      <Layout pageTitle={pageTitle}>
-        <div className={styles.content}>
-                <StaticImage src={"../images/lizzie_and_aloy_round.png"} placeholder="blurred" quality={100} />
-                <h1 className={styles.title}>Thank you!</h1>
-                <h2 className={styles.subtitle}>Check your inbox for a confirmation email.</h2>
-                <h3 className={styles.heading}>Don't forget to check your spam folder!</h3>
-        </div>
-      </Layout>
+            params['thank_you'] ? (
+                <Layout pageTitle={pageTitle}>
+                    <div className={styles.content}>
+                        <StaticImage src={"../images/lizzie_and_aloy_round.png"} placeholder="blurred" quality={100} />
+                        <h1 className={styles.title}>Thank you!</h1>
+                        <h2 className={styles.subtitle}>Check your inbox for a confirmation email.</h2>
+                        <h3 className={styles.heading}>Don't forget to check your spam folder!</h3>
+                    </div>
+            </Layout>
+            ) : (<div/>)
   )
 }
 
