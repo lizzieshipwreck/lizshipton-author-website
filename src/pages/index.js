8985ae686ad2617add29b27de8958c50f7996e2b
lizzieshipwreck <lizzieshipwreck@gmail.com>
2022-07-22 09:31:13 -0700

    fix: remove unecessary import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 import * as styles from './index.module.css'
@@ -8,18 +7,18 @@ const IndexPage = () => {
 
   return (
     <Layout pageTitle={"Home"}>
-      <div className={styles.content}>
-        <a href="https://subscribepage.io/scourge-giveaway">
-          <h1 className={styles.titleText}><b>Scourge</b> novella available now</h1>
-          <StaticImage
-            src="../images/scourge_mockup.jpg"
-            width={1000}
-            placeholder="blurred"
-            quality={100}
-          />
-          <h2 className={styles.text}>Click to download for FREE!</h2>
-        </a>
-      </div>
+        <div className={styles.content}>
+          <a href="https://subscribepage.io/scourge-giveaway">
+            <h1 className={styles.titleText}><b>Scourge</b> novella available now</h1>
+            <StaticImage
+              src="../images/scourge_mockup.jpg"
+              width={1000}
+              placeholder="blurred"
+              quality={100}
+            />
+            <h2 className={styles.text}>Click to download for FREE!</h2>
+          </a>
+        </div>
     </Layout>
   )
 }
