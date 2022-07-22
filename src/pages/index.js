2814395b6d73d54a691cc466b7de8f1293fc687a
lizzieshipwreck <lizzieshipwreck@gmail.com>
2022-07-22 13:05:34 -0700

    fix: styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,21 @@ const IndexPage = () => {
 
   return (
     <Layout pageTitle={"Home"}>
-      <div className={styles.content}>
-        <div className={styles.block}>
-          <div className={styles.stack}>
-            <StaticImage src={"../images/scourge_cover_small.jpg"} placeholder="blurred" quality={100} height={600} />
-            <Link to="/free-novella">
-              <button className={styles.buttonScourge}>GET YOUR FREE COPY</button>
-            </Link>
+        <div className={styles.content}>
+          <div className={styles.block}>
+            <div className={styles.stack}>
+              <StaticImage src={"../images/scourge_cover_small.jpg"} placeholder="blurred" quality={100} height={600} />
+              <Link to="/free-novella">
+                <button className={styles.buttonScourge}>GET YOUR FREE COPY</button>
+              </Link>
+            </div>
+            <div className={styles.stack}>
+              <StaticImage src={"../images/salt_cover_small.jpg"} placeholder="blurred" quality={100} height={600} />
+              <Link to="/newsletter">
+                <button className={styles.buttonSalt}>GET RELEASE UPDATES</button>
+              </Link>
+            </div>
           </div>
-          <div className={styles.stack}>
-            <StaticImage src={"../images/salt_cover_small.jpg"} placeholder="blurred" quality={100} height={600} />
-            <Link to="/newsletter">
-              <button className={styles.buttonSalt}>GET RELEASE UPDATES</button>
-            </Link>
-          </div>
-        </div>
       </div>
     </Layout>
   )
