5e40ea3bba3beffb6370009ba3d7962bb6d85ce1
lizzieshipwreck <lizzieshipwreck@gmail.com>
2022-08-24 12:30:57 -0500

    fix: typo

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,14 +17,12 @@ const AboutPage = () => {
               We're on a mission to sail around the world, and I'm turning my real-life adventures into YA Sci-Fi books!
             </p>
             <p className={styles.text}>
-              My debut series is about two friends who are forced to undertake an arduous sailng voyage in a 
+              My debut series is about two friends who are forced to undertake an arduous sailing voyage in a 
               near-future dystopia. It deals with themes that are important to me: identity, mental health,
-              climate change, immigration, and figuring out your place in the world.
-              I love incorporating the incredible, beautiful, fascinating places and people we encounter into my work.
+              climate change, inequality, and figuring out your place in the world.
             </p>
             <p className={styles.text}>
-              I write what I know.
-              I write about sailing because it helps me process my own sailing experiences.
+              I write about sailing because it helps me process my own sailing experiences. I love incorporating the incredible, beautiful, fascinating places and people we encounter into my work.
               I write about characters who struggle with addiction and mental health because these are things I have struggled with.
               I write about the immigrant experience because I grew up as an immigrant.
               I write about climate change, pandemics, and the apocalypse because...honestly? It's more fun to read about those things in a book 
@@ -32,7 +30,7 @@ const AboutPage = () => {
               I write about pirates because...well, pirates. Duh.
             </p>
             <p className={styles.text}>
-              When I'm not writing about young adults and the impending apocalypse, I code or write about code.
+              When I'm not writing about the impending apocalypse, I code or write about code.
               You can also sometimes find me doing yoga, walking my dog, singing, or watching Taskmaster.
             </p>
             <h3 className={styles.textFooter}> ⚓ Thanks for stopping by!</h3>
