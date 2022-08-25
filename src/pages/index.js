738f586a77577217ab2b657fca5e0da251d20c91
lizzieshipwreck <lizzieshipwreck@gmail.com>
2022-08-25 11:43:20 -0500

    fix: book descriptions

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import Layout from '../components/layout'
 import * as styles from './index.module.css'
 
 const scourge_one = "Marlin Paz and her girlfriend Gnamma Farr arrive in the harbor city of Brume after twenty-five days on the Salt, and are forbidden to come ashore."
-const scourge_two = "Axioma Nation, CEO of the world's leading corporation, receives an urgent message from a colleague overseas that makes the climate crisis the least of her concerns."
-const scourge_three = "Heron Howsley must reckon with her family's legacy and her own demons while facing down tornados and discovering that her love for her hometown runs deeper than she knew."
+const scourge_two = "Meanwhile, Axioma Nation, CEO of the world's leading corporation, receives an urgent message from a colleague overseas that makes the climate crisis the least of her concerns."
+const scourge_three = "And Heron Howsley must reckon with her family's legacy and her own demons while facing down tornados and discovering that her love for her hometown runs deeper than she knew."
 
 const salt_one = "When Bird drinks, she blips. And lately, she's been blipping a lot."
 const salt_two = "Bird Howsley and Sargo Paz are struggling to find their identities in a world where biochips are ubiquitous but habitable space is at a premium."
