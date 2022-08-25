81dd4371f4c102661f22423d17de805cffda9050
lizzieshipwreck <lizzieshipwreck@gmail.com>
2022-08-25 10:15:57 -0500

    fix: book descriptions

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,14 @@ import { StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 import * as styles from './index.module.css'
 
-const scourge_one = "Marlin Paz and her girlfriend Gnamma Farr arrive in the harbor city of Brume after twenty-five days on the Salt, and are turned away from the port. What happened since they lost contact with the land?"
+const scourge_one = "Marlin Paz and her girlfriend Gnamma Farr arrive in the harbor city of Brume after twenty-five days on the Salt, and are forbidden to come ashore."
 const scourge_two = "Axioma Nation, CEO of the world's leading corporation, receives an urgent message from a colleague overseas that makes the climate crisis the least of her concerns."
 const scourge_three = "Heron Howsley must reckon with her family's legacy and her own demons while facing down tornados and discovering that her love for her hometown runs deeper than she knew."
 
 const salt_one = "When Bird drinks, she blips. And lately, she's been blipping a lot."
 const salt_two = "Bird Howsley and Sargo Paz are struggling to find their identities in a world where biochips are ubiquitous but habitable space is at a premium."
-const salt_three = "When their search for answers takes a dangerous turn, find themselves in the crosshairs of a dangerous criminal organization that wants them dead. Forced to sail into the Salt, they battle the dangers of an unpredictable ocean, and discover pirates, an underground plot, and their true feelings for each other."
+const salt_three = "When their search for answers takes a dangerous turn, find themselves in the crosshairs of a dangerous criminal organization that wants them dead."
+const salt_four = "Forced to sail into the Salt, they battle the dangers of an unpredictable ocean, and discover pirates, an underground plot, and their true feelings for each other."
 
 const IndexPage = () => {
 
@@ -46,6 +47,7 @@ const IndexPage = () => {
                   <p className={styles.description}>{salt_one}</p>
                   <p className={styles.description}>{salt_two}</p>
                   <p className={styles.description}>{salt_three}</p>
+                  <p className={styles.description}>{salt_four}</p>
                 </div>
                 <StaticImage
                   src={"../images/salt_cover_nashville_small.jpg"}
