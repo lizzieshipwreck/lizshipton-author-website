9a65bf4c9dff75b27b3413bcaa46e159858aa026
Elizabeth Shipton <elizabethshipton@Elizabeths-MacBook-Pro.local>
2023-01-30 21:36:54 -0500

    fix: linking in author block

--- src/components/index/author-intro-block.js
+++ src/components/index/author-intro-block.js
@@ -5,13 +5,15 @@ import { Link } from 'gatsby';
 
 const AuthorIntroBlock = () => {
     return (
-        <Link to="/about" className={styles.authorIntro}>
-            <StaticImage src={"../images/author_headshot_round.png"} placeholder="blurred" quality={100} height={175} className={styles.authorHeadshot} />
+        <>
+            <Link to="/about" className={styles.authorIntro}>
+                <StaticImage src={"../images/author_headshot_round.png"} placeholder="blurred" quality={100} height={175} className={styles.authorHeadshot} />
+             </Link>
             <h2 className={styles.textHeader}>Part-time author 🪶⚓ Full-time pirate</h2>
             <p className={styles.text}>
                 Sailing around the world. Turning my adventures into Sci-Fi and Fantasy books.
             </p>
-        </Link>
+            </>
     )
 }
 
