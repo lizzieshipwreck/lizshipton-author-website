8951387957ff216bf100a448e18ab5b1fbaf2a1e
Elizabeth Shipton <elizabethshipton@Elizabeths-MacBook-Pro.local>
2023-04-08 08:12:18 -0700

    ftr: update newsletter link

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import LandingPageRedirect from '../components/landing-page-redirect'
+import { seed_download_link } from '../copy/seed'
 
 const NewsletterRedirect = () => {
 
     return (
-        <LandingPageRedirect pageTitle={"Newsletter"} redirectUrl={"https://subscribepage.io/lizshipton-newsletter"}/>
+        <LandingPageRedirect pageTitle={"Newsletter"} redirectUrl={seed_download_link}/>
   )
 }
 
