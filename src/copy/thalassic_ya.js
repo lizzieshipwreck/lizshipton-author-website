5d88d379492d5fd74cb4a4311556d72bb34a72a6
Elizabeth Shipton <elizabethshipton@Elizabeths-MacBook-Pro.local>
2023-08-09 16:54:06 -0500

    fix: update link for YA paperback purchase

--- src/copy/thalassic_ya.js
+++ src/copy/thalassic_ya.js
@@ -1,3 +1,3 @@
 export const thalassic_ya_image = "ya_thalassic_complete";
 export const thalassic_ya_alt = "Covers for the complete Thalassic YA series";
-export const thalassic_ya_link = "https://forms.gle/FJrqDUxdjPenZzjs8";
+export const thalassic_ya_link = "https://forms.gle/T5QjKSBDaT51RNUX9";
