08e5b61a5b85b3f302e8c1025f349daa69511d68
Elizabeth Shipton <elizabethshipton@Elizabeths-MacBook-Pro.local>
2023-08-10 19:34:29 -0500

    fix: don't open shop in new tab

--- src/components/index/book-button.js
+++ src/components/index/book-button.js
@@ -5,7 +5,7 @@ import * as styles from "./book-button.module.css";
 const BookButton = ({ link, text, title, mobile }) => {
     return (
         <div className={`${mobile ? styles.mobileContainer : styles.container}`}>
-            <Link to={link} target="_blank">
+            <Link to={link}>
                 <button className={`${styles[`button${title}`]}`}>{text}</button>
             </Link>
         </div>
