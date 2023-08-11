24a1898051e25bbfab9360d1c4f67a295d8cba6f
Elizabeth Shipton <elizabethshipton@Elizabeths-MacBook-Pro.local>
2023-08-10 23:37:57 -0500

    fix: copy

--- src/copy/thalassic_ya.js
+++ src/copy/thalassic_ya.js
@@ -1,7 +1,7 @@
 export const thalassic_ya_alt = "Covers for the Thalassic YA series";
 export const thalassic_ya_shop_link = "https://shop.lizshipton.com/collection/ya-editions";
 
-export const about_one = "I offer two versions of my books: a Young Adult edition and a spicy New Adult edition."
+export const about_one = "I offer two editions of the Thalassic Series: a Young Adult edition and a spicy New Adult edition."
 export const about_two = "The only difference between the two editions is that the New Adult books have explicit 'spicy' (aka sex) scenes. The Young Adult editions do not include these scenes, so they are appropriate for teens and for people who prefer 'clean' romance."
 export const about_three = "The New Adult editions are available through Amazon and other online retailers. Because those retailers won't allow me to offer two versions of my series at the same time, I offer the YA editions exclusively via this website."
 export const about_four = "To purchase Young Adult eBooks or paperbacks, click the button below. If you'd prefer to read the spicy editions, head to Amazon and search for the books there."
