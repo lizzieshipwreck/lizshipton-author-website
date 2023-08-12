15b951236588e6c7eaef615335e184003376c435
Elizabeth Shipton <elizabethshipton@Elizabeths-MacBook-Pro.local>
2023-08-12 01:48:40 -0500

    fix: spicy paperback link

--- src/copy/thalassic_na.js
+++ src/copy/thalassic_na.js
@@ -1,5 +1,5 @@
 export const thalassic_na_image = "na_thalassic_complete";
 export const thalassic_na_alt = "Covers for the complete Thalassic New Adult series";
 export const thalassic_na_amazon_link = "https://www.amazon.com/dp/B0C4J9RZWS";
-export const thalassic_na_shop_link = "https://shop.lizshipton.com/collection/na-editions";
+export const thalassic_na_shop_link = "https://shop.lizshipton.com/collection/spicy-paperbacks";
 export const tropes = "homepage-transparent";
