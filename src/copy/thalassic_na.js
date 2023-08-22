d6a9eb5e8381dd9cbd1662bbb903bb33b25f2de7
Elizabeth Shipton <elizabethshipton@Elizabeths-MacBook-Pro.local>
2023-08-22 17:06:12 -0500

    fix: use legacy images until new covers are done

--- src/copy/thalassic_na.js
+++ src/copy/thalassic_na.js
@@ -1,5 +1,5 @@
-export const thalassic_na_image = "na_thalassic_complete_teaser";
+export const thalassic_na_image = "na_thalassic_legacy_complete";
 export const thalassic_na_alt = "Covers for the complete Thalassic New Adult series";
 export const thalassic_na_amazon_link = "https://www.amazon.com/dp/B0C4J9RZWS";
 export const thalassic_na_shop_link = "https://shop.lizshipton.com/collection/spicy-paperbacks";
-export const tropes = "homepage-transparent";
+export const tropes = "homepage legacy transparent";
