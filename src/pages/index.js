d316f09c5531ddbdce3e5f48a6cab1d533a568a8
Liz Shipton <lizzie.shipton@gmail.com>
2023-09-20 21:41:43 -0500

    copy: update button copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import * as styles from './index.module.css'
 import AuthorIntroBlock from '../components/index/author-intro-block';
 import SeriesImage from '../components/index/series-image';
 
-import { thalassic_na_alt, thalassic_na_image, thalassic_na_amazon_link, salt_tropes, salt_tropes_alt, savage_na_link, savage_promo, savage_promo_alt, savage_pre_order_text, savage_button_color, salt_na_link, salt_ku_text, salt_ku_mobile_text, salt_button_color, thalassic_na_cta_mobile_text, thalassic_na_cta_text, thalassic_na_button_color } from '../copy/thalassic_na';
+import { thalassic_na_alt, thalassic_na_image, thalassic_na_shop_link, salt_tropes, salt_tropes_alt, savage_na_link, savage_promo, savage_promo_alt, savage_button_text, savage_mobile_text, savage_button_color, salt_ku_link, salt_ku_text, salt_ku_mobile_text, salt_button_color, thalassic_na_cta_mobile_text, thalassic_na_cta_text, thalassic_na_button_color } from '../copy/thalassic_na';
 import BookButton from '../components/book-button';
 
 const IndexPage = () => {
@@ -49,19 +49,19 @@ const IndexPage = () => {
             <SeriesImage src={thalassic_na_image} alt={thalassic_na_alt}/>
           </div>
           <div className={styles.button}>
-            <BookButton text={thalassic_na_cta_text} mobileText={thalassic_na_cta_mobile_text} title={thalassic_na_button_color} link={thalassic_na_amazon_link}/>
+            <BookButton text={thalassic_na_cta_text} mobileText={thalassic_na_cta_mobile_text} title={thalassic_na_button_color} link={thalassic_na_shop_link}/>
           </div>
           <div ref={midAnimatedElement} className={`${styles.middle} ${shouldAnimateMid && styles.inView}`}>
             <SeriesImage src={savage_promo} alt={savage_promo_alt}/>
           </div>
           <div className={styles.button}>
-            <BookButton text={savage_pre_order_text} mobileText={savage_pre_order_text} title={savage_button_color} link={savage_na_link}/>
+            <BookButton text={savage_button_text} mobileText={savage_mobile_text} title={savage_button_color} link={savage_na_link}/>
           </div>
           <div ref={endAnimatedElement} className={`${styles.end} ${shouldAnimateEnd && styles.inView}`}>
             <SeriesImage src={salt_tropes} alt={salt_tropes_alt}/>
           </div>
           <div className={styles.button}>
-            <BookButton text={salt_ku_text} mobileText={salt_ku_mobile_text} title={salt_button_color} link={salt_na_link}/>
+            <BookButton text={salt_ku_text} mobileText={salt_ku_mobile_text} title={salt_button_color} link={salt_ku_link}/>
           </div>
       </div>
     </Layout>
