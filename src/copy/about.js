4477ef474006197ac219c4a4ddaab9a6c1761727
Liz Shipton <lizzie.shipton@gmail.com>
2023-12-03 10:44:32 -0400

    fix: copy

--- src/copy/about.js
+++ src/copy/about.js
@@ -1,7 +1,7 @@
 const about_liz_heading = "Part-time author 🪶⚓ Full-time pirate";
 const about_liz_image = "liz hammock smile round";
 const about_liz_alt = "Liz working from a hammock";
-const about_liz_one = "Hi! I'm Liz. I'm a freelance writer, indie author, and full-time, off-grid, live-aboard sailor. I’m currently sailing around the world with my boyfriend and my dog, turning my real-life adventures into Sci-Fi and Fantasy books.";
+const about_liz_one = "Hi! I'm Liz. I'm a freelance writer, indie author, and full-time, off-grid, live-aboard sailor. I’m currently sailing around the world with my boyfriend and my dog, turning my real-life adventures into speculative fiction.";
 const about_liz_two = "I love incorporating the incredible experiences, beautiful places, and fascinating people I encounter on my travels into my work. Many parts of the world are underrepresented in fiction, particularly speculative fiction, and I feel grateful to be able to explore them."
 const about_liz_three = "I also use my books as a means to explore themes of mental health, addiction, technology, climate change, and the looming collapse of society (but, like...in a fun way.)"
 const about_liz_four = "When I'm not penning novels about the impending apocalypse, I work as a freelance content writer specializing in articles about code, music theory, and off-grid living.\nOn the rare occasion I'm not writing, you can find me swimming, hiking, telling my dog I love her for the bazillionth time today, or watching Taskmaster."
