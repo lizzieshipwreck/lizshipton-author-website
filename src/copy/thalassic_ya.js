e425e2946d606a466e0bc1b9bf1ef8709ee783e9
Liz Shipton <lizzie.shipton@gmail.com>
2023-12-06 15:53:39 -0400

    fix: copy

--- src/copy/thalassic_ya.js
+++ src/copy/thalassic_ya.js
@@ -8,7 +8,7 @@ const thalassic_ya_button_color = "Scourge";
 
 const thalassic_ya_one = "I offer two editions of the Thalassic Series: a New Adult edition and a Young Adult edition."
 const thalassic_ya_two = "The only difference between the two editions is that the New Adult books have explicit 'spicy' scenes. The Young Adult editions do not include these scenes, so they are appropriate for older teens (16+) and for people who prefer 'clean' romance."
-const thalassic_ya_three = "Amazon and other online retailers won't allow me to offer two versions of my series at the same time, so I offer the non-spicy YA editions exclusively via this website. Soon, I will also offer deluxe versions of the spicy NA paperbacks with maps and bonus author notes."
+const thalassic_ya_three = "Amazon and other online retailers won't allow me to offer two versions of my series at the same time, so I offer the non-spicy YA editions exclusively via this website."
 const thalassic_ya_four = "Buying direct is a great way to support indie authors! It means we get to keep more of our royalties and aren't beholden to Amazon and other retailers for our income. I appreciate your willingness to buy these books from me in this way. Thank you!"
 
 
