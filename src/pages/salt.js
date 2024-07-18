4c9ae08fa01e4f5ed601bb9a259bd285fb30272a
Liz Shipton <lizzie.shipton@gmail.com>
2024-07-18 17:42:05 -0400

    add: salt redirect page

---
+++ src/pages/salt.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import LandingPageRedirect from '../components/page-layouts/redirect-page'
+
+const Salt = () => {
+
+    return (
+        <LandingPageRedirect pageTitle={"Salt"} redirectUrl={"https://amzn.to/4cY2tnA"}/>
+  )
+}
+
+export default Salt 
