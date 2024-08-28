8fdcf840cce2f87657bdafd85dbdff56e82d9ff3
Liz Shipton <lizzie.shipton@gmail.com>
2024-08-28 13:02:40 -0400

    ftr: add dot slash redirect page

---
+++ src/pages/dot-slash-magic.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import LandingPageRedirect from '../components/page-layouts/redirect-page'
+
+const DotSlashMagic = () => {
+
+    return (
+        <LandingPageRedirect pageTitle={"Dot Slash Magic"} redirectUrl={"https://blog.lizshipton.com/dot-slash-magic"}/>
+  )
+}
+
+export default DotSlashMagic; 
