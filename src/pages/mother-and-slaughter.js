5800c1defa9c6273853903c42d6d4c931007da32
Liz Shipton <lizzie.shipton@gmail.com>
2025-02-11 11:45:34 -0400

    add: mother and slaughter

---
+++ src/pages/mother-and-slaughter.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import LandingPageRedirect from '../components/page-layouts/redirect-page'
+import { mother_and_slaughter } from '../copy/redirects';
+
+const MotherAndSlaughter = () => {
+
+    return (
+        <LandingPageRedirect pageTitle={"Mother & Slaughter"} redirectUrl={ mother_and_slaughter }/>
+  )
+}
+
+export default MotherAndSlaughter; 
