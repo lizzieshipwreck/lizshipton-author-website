c8be4dabaf7961c77ed0ba86a915471f1d606634
Liz Shipton <lizzie.shipton@gmail.com>
2025-03-10 20:33:34 -0400

    at some point you really are going to have to write some tests

--- src/components/index/series-block.js
+++ src/components/index/series-block.js
@@ -3,7 +3,7 @@ import * as styles from './series-block.module.css'
 import SeriesImage from './series-image';
 import BookButton from '../book-button';
 
-const SeriesBlock = ({image, alt, title, headlineOne, headlineTwo, cta, ctaMobile, button, link, hasBorder, isGif}) => {
+const SeriesBlock = ({image, alt, title, headlineOne, headlineTwo, cta, mobileCta, button, link, hasBorder, isGif}) => {
     return (
         <div className={styles.container}>
             {
@@ -16,7 +16,7 @@ const SeriesBlock = ({image, alt, title, headlineOne, headlineTwo, cta, ctaMobil
             <p className={`${styles.headlineText}`}>{headlineOne}</p>
             <p className={`${styles.headlineText}`}>{headlineTwo}</p>
         <div className={styles.button}>
-            <BookButton text={cta} mobileText={ctaMobile} title={button} link={link}/>
+            <BookButton text={cta} mobileText={mobileCta} title={button} link={link}/>
         </div>
     </div>
     )
