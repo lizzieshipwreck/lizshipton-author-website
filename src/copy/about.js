96365ed312062ad01908a034e4404a99f0a40c5b
Liz Shipton <lizzie.shipton@gmail.com>
2026-06-13 07:51:11 -0400

    fix: about copy

--- src/copy/about.js
+++ src/copy/about.js
@@ -6,13 +6,13 @@ export const about_liz_cta_mobile_text = "Newsletter";
 export const about_liz_button_color = "Pink";
 export const about_liz_link = "/newsletter";
 
-const about_liz_one = "I'm a speculative fiction author and loosely-hinged person. I live off-grid on a boat in the Caribbean with my partner and dog, and I spend a lot of time writing. Like. . .a lot.";
-const about_liz_two = "I try to balance dark humor, bonkers action, and epic world-building with characters rooted in my lived experience. Everything I write skews political, and many of my books contain explicit content. I like to lure you in with sex and magic, and then beat you over the head with current events."
-const about_liz_three = "If you're new to my books, start with Dot Slash Magic (explicit) or Mother & Slaughter. They have romantic subplots, but aren't considered 'romance' - for that, check out my MM pen name Hailey Reaves (explicit)."
+const about_liz_one = "I'm a speculative fiction author and loosely-hinged person.";
+const about_liz_two = "I started writing when my partner, dog, and I set sail from California in 2021. My first book, Salt, was written on my phone while I was in the cockpit on night watch along the Pacific Coast. That series includes a lot of real-life adventures and locations, as do all my books since."
+const about_liz_three = "As a writer, I try to balance dark humor, bonkers action, and epic world-building with characters rooted in my lived experience. My work skews political, and many of my books contain explicit content. I like to lure you in with sex and magic, and then beat you over the head with current events. Immigration, feminism, queer rights, mental health visibility, and Donald Trump are common themes."
 const about_liz_four = "On the rare occasion I'm not writing, you can find me at yoga or karaoke, complaining about the weather, telling my dog I love her for the bazillionth time today, or watching 8 Out of 10 Cats Does Countdown."
 const about_liz_five = "she/they"
 
-const about_loki_subheading = "Full time author 🪶⚓ Part time pirate";
+const about_loki_subheading = "Author 🪶⚓ Pirate";
 const about_loki_image = "loki";
 const about_loki_link = "https://instagram.com/sv_loki";
 const about_loki_alt =  "Liz's boat Loki";
