d441dd34920ccdd2c4792fd72482b75671d31948
Liz Shipton <lizzie.shipton@gmail.com>
2026-06-19 12:04:57 -0400

    fix: copy

--- src/copy/newsletter.js
+++ src/copy/newsletter.js
@@ -15,7 +15,7 @@ export const NEWSLETTER_INDEX_PAGE = {
 
 const newsletter_header = "Join the Crew"
 const newsletter_text_one = "Sign up for my free monthly newsletter to download the first chapter of Mother & Slaughter for free."
-const newsletter_text_two = "You'll also get exclusive recommendations, ARC opportunities, updates about my work and travel, and pictures of my dog. Plus, optional access to my weekly Substack essays about the magic and mayhem of being an artist in today's algorithm-driven world."
+const newsletter_text_two = "You'll also get exclusive recommendations, ARC opportunities, updates about my work and travel, and pictures of my dog. Tap through the steps and check your inbox to confirm your email address. Make sure to check the spam folder too!"
 
 export const content = {
     newsletter_header,
